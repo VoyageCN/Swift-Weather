a14dc793c69adc089069f3a2444880bc04d7b6a5
VoyageCN <VoyageCN@VoyageCN.local>
2016-07-24 17:09:12 +0800

    git init

--- Swift Weather/ViewController.swift
+++ Swift Weather/ViewController.swift
@@ -7,18 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager:CLLocationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if ios9() {
+            locationManager.requestAlwaysAuthorization()
+        }
+        locationManager.startUpdatingLocation()
+    }
+    
+    func ios9() -> Bool {
+        return UIDevice.currentDevice().systemVersion == "9.3"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var location:CLLocation = locations[locations.count-1] as CLLocation
+        
+        if location.horizontalAccuracy > 0 {
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
+    
 
 
 }
